c108ac2a7f6cb223f6addf96a306270ca51e759c
mafdysaad <mafdysaad366@gmail.com>
2025-03-10 13:46:28 +0200

    add lineartoeastout animate

--- lib/presention/screen/home/widget/view_body.dart
+++ lib/presention/screen/home/widget/view_body.dart
@@ -1,63 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/core/utils/assets.dart';
-import 'package:portfolio/core/utils/fontstyle.dart';
 
-class ViewBody extends StatelessWidget {
+import 'package:portfolio/presention/screen/home/widget/background_image.dart';
+import 'package:portfolio/presention/screen/home/widget/custom_shape.dart';
+import 'package:portfolio/presention/screen/home/widget/details.dart';
+
+class ViewBody extends StatefulWidget {
   const ViewBody({super.key});
+
+  @override
+  State<ViewBody> createState() => _ViewBodyState();
+}
+
+class _ViewBodyState extends State<ViewBody> with TickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> animation_1;
+  late Animation<Offset> animation_2;
+  @override
+  void initState() {
+    initanimation();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Positioned(
-          child: SizedBox(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              child: FittedBox(
-                  fit: BoxFit.fill,
-                  child: Image.asset(Assets.resourceImagesFinal))),
-        ),
-        Positioned(
-            bottom: 0,
-            child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                child: FittedBox(
-                    fit: BoxFit.cover,
-                    child: Image.asset(
-                        color: const Color.fromARGB(255, 123, 124, 124),
-                        Assets.resourceImagesRectangle)))),
-        Positioned(
-            bottom: 40,
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    spacing: 4,
-                    children: [
-                      Text(
-                        'Hi, I am',
-                        style: Style.soyombo18_sembold(),
-                      ),
-                      Text('Mafdy Saad', style: Style.soyombo20_bold()),
-                      Text('Front-end Developer / UI Designer',
-                          style: Style.soyombo18_sembold()),
-                    ],
-                  ),
-                  Column(
-                    spacing: 29,
-                    children: [
-                      Image.asset(Assets.resourceImagesMail),
-                      Image.asset(Assets.resourceImagesGethup),
-                      Image.asset(Assets.resourceImagesLinkedin),
-                    ],
-                  )
-                ],
-              ),
-            ))
+        Positioned(child: BackgroundImage()),
+        Positioned(bottom: 0, child: CustomShape(animation: animation_1)),
+        Positioned(bottom: 40, child: Details(animation_2: animation_2))
       ],
     );
+  }
+
+  initanimation() {
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 3))
+          ..forward();
+
+    animation_1 = Tween<Offset>(begin: Offset(0, 1.5), end: Offset(0, 0))
+        .animate(CurvedAnimation(
+            parent: animationController, curve: Curves.bounceInOut));
+    animation_2 = Tween<Offset>(begin: Offset(1.5, 0), end: Offset(0, 0))
+        .animate(CurvedAnimation(
+            parent: animationController,
+            curve: Interval(.5, 1, curve: Curves.linearToEaseOut)));
   }
 }
