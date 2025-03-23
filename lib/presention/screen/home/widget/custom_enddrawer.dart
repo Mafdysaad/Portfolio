090fe084ece00f39e5e0e3777f9739d5da513604
mafdysaad <mafdysaad366@gmail.com>
2025-03-24 01:12:42 +0200

    after that

--- lib/presention/screen/home/widget/custom_enddrawer.dart
+++ lib/presention/screen/home/widget/custom_enddrawer.dart
@@ -44,24 +44,6 @@ class _MyWidgetState extends State<CustomEnddrawer> {
           ]),
         ),
       ),
-      SliverToBoxAdapter(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Stack(children: [
-            Positioned.fill(
-                child: Image(
-                    image: AssetImage(Assets.resourceImagesBackground),
-                    fit: BoxFit.fill)),
-            Positioned.fill(
-              child: AnimatedOpacity(
-                  opacity: _opacity,
-                  duration: Duration(milliseconds: 500),
-                  child: PersonalInfo()),
-            )
-          ]),
-        ),
-      )
     ]);
   }
 }
